5cd97df44d4ec0469e0948bda98421460599b362
Kandy Foma <kandy@siege.ai>
2019-03-18 16:23:09 +0200

    Auth example

--- DjangoWeb/DjangoWeb/DjangoWeb/urls.py
+++ DjangoWeb/DjangoWeb/DjangoWeb/urls.py
@@ -5,6 +5,9 @@ Definition of urls for DjangoWeb.
 from datetime import datetime
 from django.conf.urls import url
 import django.contrib.auth.views
+from django.conf.urls import include
+from django.contrib import admin
+admin.autodiscover()
 
 import app.forms
 import app.views
@@ -34,13 +37,14 @@ urlpatterns = [
     url(r'^logout$',
         django.contrib.auth.views.logout,
         {
-            'next_page': '/',
+            'template_name': 'app/loggedoff.html',
+            # 'next_page': '/',
         },
         name='logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ]
