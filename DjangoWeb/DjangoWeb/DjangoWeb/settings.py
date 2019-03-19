6404757deda10c8f25552d5bf0ff197a46f51bda
Kandy Foma <kandy@siege.ai>
2019-03-19 12:45:50 +0200

    Playing with the API

--- DjangoWeb/DjangoWeb/DjangoWeb/settings.py
+++ DjangoWeb/DjangoWeb/DjangoWeb/settings.py
@@ -33,8 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
-    'polls',
-    # Add your apps here to enable them
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
