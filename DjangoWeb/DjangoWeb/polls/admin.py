581716d1b5c23d938fb5c8aa0f8cd8aa84b36330
Kandy Foma <kandy@siege.ai>
2019-03-22 13:29:01 +0200

    fixed trhe models

--- DjangoWeb/DjangoWeb/polls/admin.py
+++ DjangoWeb/DjangoWeb/polls/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
 admin.site.register(Question)
 admin.site.register(Choice)
