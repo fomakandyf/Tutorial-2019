b19fbb0a6a24da2a95a1f77dd7998101afb01c3d
Kandy Foma <kandy@siege.ai>
2019-03-22 13:32:54 +0200

    Rearranginfg admin fields

--- DjangoWeb/DjangoWeb/polls/admin.py
+++ DjangoWeb/DjangoWeb/polls/admin.py
@@ -3,5 +3,5 @@ from polls.models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
