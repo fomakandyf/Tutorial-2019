0e14090542758c47e2695c3dd0780e7873331945
Kandy Foma <kandy@siege.ai>
2019-03-22 13:44:06 +0200

    Inline ForeignKey

--- DjangoWeb/DjangoWeb/polls/admin.py
+++ DjangoWeb/DjangoWeb/polls/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
+
+class ChoiceInline(admin.StackedInline):
+
+      model= Choice
+      extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information',{'fields':['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
